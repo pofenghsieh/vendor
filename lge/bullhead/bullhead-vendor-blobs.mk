1fc1e67df45116d0aa92e9f91f9f303cde8ecb25
Daniel Micay <danielmicay@gmail.com>
2016-10-03 13:39:31 -0400

    Nexus 5X NBD90W vendor files

--- lge/bullhead/bullhead-vendor-blobs.mk
+++ lge/bullhead/bullhead-vendor-blobs.mk
@@ -1,4 +1,4 @@
-# [2016-09-27] Auto-generated file, do not edit
+# [2016-10-03] Auto-generated file, do not edit
 
 PRODUCT_COPY_FILES += \
     vendor/lge/bullhead/proprietary/bin/ATFWD-daemon:$(TARGET_COPY_OUT_SYSTEM)/bin/ATFWD-daemon:lge \
